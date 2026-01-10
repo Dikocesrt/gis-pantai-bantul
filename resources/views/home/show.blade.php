6da5c1e13fa6a944d31d0072256d3b466b21ee0a
Dikocesrt <dikocesrt@gmail.com>
2026-01-10 12:10:42 +0700

    feat(road-accessibililty): added information for road accessibility on detail tempat wisata

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -146,8 +146,38 @@
                                                 </path>
                                             </svg>
                                         </div>
-                                        <div>
-                                            <p class="text-xs font-medium text-gray-600 mb-1">Aksesibilitas Jalan</p>
+                                        <div class="flex-1">
+                                            <div class="flex items-center gap-2">
+                                                <p class="text-xs font-medium text-gray-600 mb-1">Aksesibilitas Jalan</p>
+                                                <div class="relative group">
+                                                    <svg class="w-4 h-4 text-gray-500 cursor-help" fill="currentColor"
+                                                        viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd"
+                                                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                                            clip-rule="evenodd"></path>
+                                                    </svg>
+                                                    <div
+                                                        class="absolute left-0 bottom-full mb-2 hidden group-hover:block w-64 p-3 bg-gray-900 text-white text-sm rounded-lg shadow-xl z-10">
+                                                        <div class="relative">
+                                                            @if ($tempatWisata->road_accessibility == 'mudah_diakses')
+                                                                Semua jenis kendaraan (motor, mobil, bus) dapat dengan mudah
+                                                                mencapai lokasi. Jalan beraspal dan kondisi baik.
+                                                            @elseif($tempatWisata->road_accessibility == 'cukup_mudah')
+                                                                Kendaraan kecil (motor, mobil) mudah akses. Kendaraan besar
+                                                                (bus, truk) perlu kehati-hatian ekstra karena jalan yang
+                                                                agak sempit atau berkelok.
+                                                            @elseif($tempatWisata->road_accessibility == 'sulit_diakses')
+                                                                Akses terbatas, lebih cocok untuk motor atau kendaraan
+                                                                kecil. Jalan mungkin sempit, berbatu, atau belum beraspal
+                                                                sempurna.
+                                                            @endif
+                                                            <div
+                                                                class="absolute left-4 top-full w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-gray-900">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                             <p class="text-sm font-bold text-gray-900">
                                                 {{ \App\Models\TempatWisata::ROAD_ACCESSIBILITY[$tempatWisata->road_accessibility] ?? $tempatWisata->road_accessibility }}
                                             </p>
